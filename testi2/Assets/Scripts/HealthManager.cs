cb19e0fa7d4169086c47b2b174c86cd930590cb5
Jyri Lindroos <jyri.lindroos@keuda.fi>
2019-03-01 08:26:31 +0200

    tasohyppely

---
+++ testi2/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour //NewBehaviourScript : MonoBehaviour
+{
+    public GameObject[] hearts;
+    private int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = hearts.Length;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
